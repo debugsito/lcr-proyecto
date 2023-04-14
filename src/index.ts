e7189fbba14b791d42de833dc99f540a8aa12661
debugsito <csramosflores@gmail.com>
2023-04-14 11:21:49 -0500

    other services comment

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import AWS from "aws-sdk";
 import config from "./config";
 
 AWS.config.update({ region: config.dynamodb.region, });
-
 const lambda = new AWS.Lambda();
+const dynamodb = new AWS.DynamoDB();
+const sqs = new AWS.SQS();
+const eventbridge = new AWS.EventBridge();
 
 const functionName = "lcrFunction";
 const handler = "handler";
@@ -12,6 +14,11 @@ const handler = "handler";
 async function main() {
   const redisClient = RedisClientSingleton.getInstance();
   await redisClient.connect();
+  // await createTable();
+  // await createSQS();
+  // await createEventBridge();
+  // await createSQS();
+  // await crearDisparador();
 
   const data = await redisClient.getListData('mi_lista');
   console.log("listas",data);
@@ -21,7 +28,8 @@ async function main() {
     let result_function = await invokeFunction(row);
     if(result_function!==false){
       let respuesta = JSON.parse(<string>result_function.Payload);
-      console.log('respuesta:', respuesta);
+      console.log(respuesta)
+      // await sendEvent(row,<string>result_function.Payload);
     }
   }
 
@@ -42,6 +50,113 @@ async function invokeFunction(payload: string) {
     console.error(err);
     return false;
   }
+}
+
+async function crearDisparador(){
+  const params = {
+    FunctionName: 'saveEventsDynamo',
+    BatchSize: 10,
+    EventSourceArn: 'arn:aws:sqs:us-east-2:042773476994:colaLCR',
+  };
+
+  lambda.createEventSourceMapping(params, (err, data) => {
+    if (err) {
+      console.log('Error creating event source mapping:', err);
+    } else {
+      console.log('Event source mapping created successfully:', data);
+    }
+  });
+}
+
+async function sendEvent(input:string,output:string){
+  const params = {
+    Entries: [
+      {
+        Source: 'lcrFunction',
+        DetailType: 'newGame',
+        Detail: JSON.stringify({
+          // id: 'miId',
+          timestamp: Date.now(),
+          input,
+          output,
+        }),
+      },
+    ],
+  };
+
+  eventbridge.putEvents(params, (err, data) => {
+    if (err) {
+      console.log('Error putting events:', err);
+    } else {
+      console.log('Events put successfully:', data);
+    }
+  });
+}
+async function createEventBridge(){
+  const params = {
+    Name: 'ruleLCR',
+    EventPattern: {
+      source: ['miFuente'],
+    },
+    State: 'ENABLED',
+    Targets: [
+      {
+        Arn: 'arn:aws:sqs:us-east-2:042773476994:ruleLCR',
+        Id: 'miTarget',
+      },
+    ],
+  };
+
+  // @ts-ignore
+  eventbridge.putRule(params, (err, data) => {
+    if (err) {
+      console.log('Error creating rule:', err);
+    } else {
+      console.log('Rule created successfully:', data.RuleArn);
+    }
+  });
+}
+async function createSQS(){
+  const params = {
+    QueueName: 'colaLCR',
+  };
+
+  sqs.createQueue(params, (err, data) => {
+    if (err) {
+      console.log('Error creating queue:', err);
+    } else {
+      console.log('Queue created successfully:', data.QueueUrl);
+    }
+  });
+}
+async function createTable(){
+  const params = {
+    TableName: 'miTabla',
+    KeySchema: [
+      { AttributeName: 'id', KeyType: 'HASH' },
+      { AttributeName: 'input', KeyType: 'HASH' },
+      { AttributeName: 'output', KeyType: 'HASH' },
+      { AttributeName: 'timestamp', KeyType: 'RANGE' },
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'id', AttributeType: 'S' },
+      { AttributeName: 'input', AttributeType: 'S' },
+      { AttributeName: 'output', AttributeType: 'S' },
+      { AttributeName: 'timestamp', AttributeType: 'N' },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5,
+    },
+  };
+
+  dynamodb.createTable(params, (err, data) => {
+    if (err) {
+      console.log('Error creating table:', err);
+    } else {
+      console.log('Table created successfully:', data);
+    }
+  });
 }
 
 main().catch(err => console.error(err));
